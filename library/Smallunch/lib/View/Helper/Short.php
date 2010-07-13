58d8f71193af38b76be9beb08985616daaed3a70
Jeremy Moore <boetechnologies@gmail.com>
2010-07-13 14:11:35 -0500

    Added Comments default value added to $chars $ending param added to function

--- library/Smallunch/lib/View/Helper/Short.php
+++ library/Smallunch/lib/View/Helper/Short.php
@@ -2,16 +2,20 @@
 class Smallunch_lib_View_Helper_Short extends Zend_View_Helper_FormSelect
 {
   /**
-   * Return Categories Doctrine Array
-   * for categories that have active listings
+   * Trims $text to the number of specified $chars, 
+   * appends $ending to return value
+   * 
+   * @param $text Text to trim
+   * @param $chars Number of characters to trim to
+   * @param $ending String to append to trimmed text
    */
-  function short($text, $chars)
+  function short($text = '', $chars = 50, $ending = ' ...')
   {
   	$text = strip_tags(stripslashes($text));
     if (strlen($text) > $chars) {
     	$text = substr($text,0,$chars);
       $text = substr($text,0,strrpos($text,' '));
-      $text = $text." ...";
+      $text = $text.$ending;
     }
     return $text;
   }
