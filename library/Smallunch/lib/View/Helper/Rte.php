5138a75dc0ead4319efc13182942bd9b90b69680
Jeremy Moore <boetechnologies@gmail.com>
2010-07-13 14:12:32 -0500

    Added Comments Code cleanup Now passing $attr to formTextarea

--- library/Smallunch/lib/View/Helper/Rte.php
+++ library/Smallunch/lib/View/Helper/Rte.php
@@ -1,10 +1,14 @@
 <?php
 class Smallunch_lib_View_Helper_Rte extends Zend_View_Helper_Abstract
 {
+  /**
+   * Returns CKEditor form field
+   * @param string $name  Name used for generated field
+   * @param string $value Content for CKEditor
+   * @param array $attr Attributes for textarea
+   */
   function rte($name, $value = null, $attr = array())
   {
-  	#require_once 'Zend/View/Helper/FormTextarea.php';
-  	#$textarea = new Zend_View_Helper_FormTextarea();
     $baseUrl = str_replace('/index.php', '', str_replace('/dev.php', '', Zend_Controller_Front::getInstance()->getBaseUrl()));
     
     $headLink = new Zend_View_Helper_HeadLink();
@@ -13,10 +17,9 @@ class Smallunch_lib_View_Helper_Rte extends Zend_View_Helper_Abstract
     
     $placeholder = new Zend_View_Helper_Placeholder();
     $placeholder->placeholder('jQuery')->captureStart();
-    
 	  echo "CKEDITOR.replace( '".$name."' );";
 	  $placeholder->placeholder('jQuery')->captureEnd();
 	  
-	  return $this->view->formTextarea($name, $value);
+	  return $this->view->formTextarea($name, $value, $attr);
   }
 }
