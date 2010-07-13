b1a0dea8dbd47b2480cdf49f73dfe55dfe18baa6
Jeremy Moore <boetechnologies@gmail.com>
2010-07-13 14:10:43 -0500

    Added Comments Code cleanup Added $options param

--- library/Smallunch/lib/View/Helper/UserType.php
+++ library/Smallunch/lib/View/Helper/UserType.php
@@ -1,12 +1,22 @@
 <?php
 class Smallunch_lib_View_Helper_UserType extends Zend_View_Helper_FormSelect
 {
-  function userType($name, $value = null, $attribs = null, $options = null)
+  /**
+   * Returns selectbox generated from UserRoles
+   * 
+   * @param $name Name for form element
+   * @param $value  Selected Option value
+   * @param $attribs Attributes added to the 'select' tag.
+   * @param $options 
+   */
+  function userType($name = '', $value = null, $attribs = null, $options = array())
   {
+    $roles = array();
   	$user_rs = Doctrine_Query::create()->from('UserRoles ur')->orderby('ur.name')->fetchArray();
+  	
   	foreach ($user_rs as $user) {
-  		$options[$user['id']] = $user['name'];
+  		$roles[$user['id']] = $user['name'];
   	}
-  	return $this->formSelect($name, $value, $attribs, $options);
+  	return $this->formSelect($name, $value, $attribs, array_merge($options, $roles));
   }
 }
